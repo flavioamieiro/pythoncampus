dd1427fb0221801961ef1082faf6aa40b033f0a7
Henrique Bastos <henrique@bastos.net>
2010-01-20 17:56:52 -0200

    Added static pages using direct_to_template

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,27 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
+
+)
+
+urlpatterns+= patterns('django.views.generic.simple',
+    (r'^$', 'direct_to_template', {'template': 'about.html'}),
+
+    (r'^sobre/$', 'direct_to_template', {'template': 'about.html'}),
+
+    (r'^eu-quero-uma-pythoncampus-na-minha-universidade/$',
+        'direct_to_template', {'template': 'i-want.html'}),
+
+    (r'^contato/$', 'direct_to_template', {'template': 'contact.html'}),
 )
