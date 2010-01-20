c11cb86a74a5b823136c306c8c269101ab5a6404
Henrique Bastos <henrique@bastos.net>
2010-01-20 01:55:51 -0200

    Added initial django project

---
+++ passenger_wsgi.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+PROJECT_ROOT = os.path.dirname(__file__)
+
+sys.path.insert(0, PROJECT_ROOT)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'lib'))
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'src'))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
