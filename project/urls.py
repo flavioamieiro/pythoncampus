b1afa8a6c3c2fb53786bed98206b77b7fd31b0d1
Henrique Bastos <henrique@bastos.net>
2010-01-27 02:29:15 -0200

    Added making-of page

--- project/urls.py
+++ project/urls.py
@@ -25,6 +25,8 @@ urlpatterns+= patterns('django.views.generic.simple',
         'direct_to_template', {'template': 'i-want.html'}),
 
     (r'^contato/$', 'direct_to_template', {'template': 'contact.html'}),
+    
+    (r'^making-of/$', 'direct_to_template', {'template': 'making-of.html'}),
 )
 
 if settings.DEBUG:
