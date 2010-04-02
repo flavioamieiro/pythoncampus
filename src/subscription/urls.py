3bcc35539e7795f64c2936cef42e70781ebf81bf
Henrique Bastos <henrique@bastos.net>
2010-04-02 16:40:43 -0300

    Removed unused url configs from subscription.urls

--- src/subscription/urls.py
+++ src/subscription/urls.py
@@ -6,12 +6,5 @@ from views import new, create, index, confirm_email
 
 urlpatterns = patterns('',
     route(r'^signup/$', GET=new, POST=create, name='signup'),
-    url(r'^$', index, name='signatures-list'),
     url(r'^signup/confirm_email/(\w+)/$', confirm_email, name='confirm_email'),
 )
-
-#if settings.DEBUG:
-#    urlpatterns += patterns('django.views.generic.simple',
-#        (r'^signed/$', 'direct_to_template', {'template': 'signatures/signed.html'},
-#            {'MEDIA_URL': settings.MEDIA_URL}),
-#    )
