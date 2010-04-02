1c0f1bf4f1e36de7d3a0a8811e44cdd2bfaf2169
Sprint <sprint@administrator-desktop.(none)>
2010-04-02 18:23:42 -0300

    Cria a página de descrição da oficina

--- project/urls.py
+++ project/urls.py
@@ -23,6 +23,8 @@ urlpatterns+= patterns('django.views.generic.simple',
     (r'^contato/$', 'direct_to_template', {'template': 'contact.html'}),
     
     (r'^making-of/$', 'direct_to_template', {'template': 'making-of.html'}),
+
+    (r'^oficina/$', 'direct_to_template', {'template': 'oficina.html'}),
 )
 
 if settings.DEBUG:
