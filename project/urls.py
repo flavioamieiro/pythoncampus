fb0c7eadc7c0c61b29ecaad746e72ec95a9bac69
Henrique Bastos <henrique@bastos.net>
2010-05-03 21:28:47 -0300

    Added session agenda

--- project/urls.py
+++ project/urls.py
@@ -14,9 +14,49 @@ urlpatterns = patterns('',
     (r'', include('server.urls')),
 )
 
-talks = ({
+talks = (
+    {
+    'title': 'Abertura',
+    'time': '9:00',
+    },
+    {
+    'title': '"Uma Experiência Acadêmica com Python"',
+    'time': '9:20',
+    'description': """
+    Relato da experiência de uso da linguagem Python no Núcleo de Pesquisa em Sistemas de Informação e no ensino de programação dos cursos de computação do Instituto Federal Fluminense.
+    """,
+    'speaker_name': 'Fábio Duncan',
+    'speaker_about': """
+    Possui mestrado em Pesquisa Operacional e Inteligência Computacional pela Universidade Candido Mendes (Campos - 2008).<br />
+    Atualmente é professor do Instituto Federal Fluminense e membro do Núcleo de Pesquisa em Sistemas de Informação (NSI-IFF). Tem experiência na área de Ciência da Computação com ênfase em algoritmos genéticos, programação paralela e segmentação de vídeo.
+    """,
+    'speaker_url': '',
+    'speaker_img': '/media/images/fabio_duncan.jpg',
+    },
+    {
+    'title': 'Palestra Técnica',
+    'time': '10:20',
+    },
+    {
+    'title': 'Coffee Break Reforçado',
+    'time': '11:20',
+    },
+    {
+    'title': '"Computação visual com Python"',
+    'time': '12:00',
+    'description': """
+    Você é da área de tecnologia? Acredite, você está em muita vantagem!<br />
+    Conheça como funciona a sociedade e a organização do trabalho, e saiba como a Internet, o Software Livre e tecnologias como Python e Ruby podem revolucionar a sua vida.<br />
+    Aprenda a hackear o "ciclo vicioso social" e ter as rédeas o teclado da sua vida, nas suas mãos.<br />
+    """,
+    'speaker_name': 'Jonh Edson Ribeiro de Carvalho',
+    'speaker_about': 'Graduado [1997] em Física pela UFF e Mestrado [2006] em Computação também pela UFF, com Ênfase em Computação Visual e Interfaces. Professor Universitário a 10 anos, entusiasta do Movimento de Software Livre, sendo membro da SL-RJ, Debian-RJ e da PythOnRio e apoia as Culturas Livres.',
+    'speaker_url': 'http://www.visual.pro.br/',
+    'speaker_img': 'http://a1.twimg.com/profile_images/326997520/closecabofrio2_bigger.jpg',
+    },
+    {
     'title': '"Fuja da escravidão, antes que ela te alcance."',
-    'time': '14:00',
+    'time': '13:00',
     'description': """
     Você é da área de tecnologia? Acredite, você está em muita vantagem!<br />
     Conheça como funciona a sociedade e a organização do trabalho, e saiba como a Internet, o Software Livre e tecnologias como Python e Ruby podem revolucionar a sua vida.<br />
@@ -28,18 +68,9 @@ talks = ({
     'speaker_img': 'http://devinrio.com.br/img/palestrante_vinicius_teles.jpg',
     },
     {
-    'title': '"Computação visual com Python"',
-    'time': '13:00',
-    'description': """
-    Você é da área de tecnologia? Acredite, você está em muita vantagem!<br />
-    Conheça como funciona a sociedade e a organização do trabalho, e saiba como a Internet, o Software Livre e tecnologias como Python e Ruby podem revolucionar a sua vida.<br />
-    Aprenda a hackear o "ciclo vicioso social" e ter as rédeas o teclado da sua vida, nas suas mãos.<br />
-    """,
-    'speaker_name': 'Jonh Edson Ribeiro de Carvalho',
-    'speaker_about': 'Graduado [1997] em Física pela UFF e Mestrado [2006] em Computação também pela UFF, com Ênfase em Computação Visual e Interfaces. Professor Universitário a 10 anos, entusiasta do Movimento de Software Livre, sendo membro da SL-RJ, Debian-RJ e da PythOnRio e apoia as Culturas Livres.',
-    'speaker_url': 'http://www.visual.pro.br/',
-    'speaker_img': 'http://a1.twimg.com/profile_images/326997520/closecabofrio2_bigger.jpg',
-    },)
+    'title': 'Palestras Relâmpago',
+    'time': '14:00',
+    })
 
 
 urlpatterns+= patterns('django.views.generic.simple',
