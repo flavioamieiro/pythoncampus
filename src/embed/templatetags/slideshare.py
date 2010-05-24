334b935ef39951e98f316144abf5283dcccb421f
Henrique Bastos <henrique@bastos.net>
2010-05-24 01:01:53 -0300

    Created an "embed" app to hold a slideshare helper.

---
+++ src/embed/templatetags/slideshare.py
@@ -0,0 +1,43 @@
+from django import template
+from django.template import Context, loader, Node
+
+
+register = template.Library()
+
+def do_slideshare(parser, token):
+    try:
+        # split_contents() knows not to split quoted strings.
+        tag_name, id_, doc = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError, "%r tag requires 2 arguments" % token.contents.split()[0]
+    return SlideShareNode(id_, doc)
+
+
+class SlideShareNode(Node):
+    def __init__(self, id_, doc):
+        self.id = id_
+        self.doc = doc
+
+    def render(self, context):
+        t = loader.get_template('embed/slideshare.html')
+        c = Context({'id': self.id, 'doc': self.doc}, autoescape=context.autoescape)
+        return t.render(c)
+
+
+register.tag('slideshare', do_slideshare)
+
+"""
+from django.template import Variable, VariableDoesNotExist
+import re
+QUOTED_STRING = re.compile(r'^["\'](?P<noquotes>.+)["\']$')
+
+def handle_var(self, value, context):
+  stringval = QUOTED_STRING.search(value)
+  if stringval:
+    return stringval.group('noquotes')
+  else:
+    try:    
+      return Variable(value).resolve(context)
+    except VariableDoesNotExist:
+      return value
+"""
